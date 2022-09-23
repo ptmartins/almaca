12715fc9a080ddaa935cb9b640249110a7d986d2
Pedro Martins <snitramordep@gmail.com>
2022-09-23 20:08:33 +0100

    Add AOS library.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 (function() {
 
     let DOM = {},
+        publications = null,
+        career = null,
 
         cacheDOM = () => {
             DOM.publications = document.querySelector('.publications');
@@ -26,23 +28,52 @@
                 return pub;
             }
         },
+
+        getData = () => {
+            fetch('publications.json').then(response => response.json()).then(data => {
+                publications = data;
+                renderPublications(publications);
+            });
+
+            fetch('career.json').then(response => response.json()).then(data => {
+                career = data;
+                renderCareer(career);
+            });
+        },
+
+        renderCareer = (data) => {
+            console.log(data);
+        },
     
-        renderPublications = () => {
-
-            let _data = null;
-            fetch('data.json').then(response => response.json()).then(data => {
-                _data = data;
-
-                console.log(_data);
-                _data.forEach(pub => {
-                    DOM.publications.append(UI.pub(pub));
-                });
+        renderPublications = (data) => {
+            data.forEach(pub => {
+                DOM.publications.append(UI.pub(pub));
             });
         },
 
         init = () => {
             cacheDOM();
-            renderPublications();
+            getData();
+            AOS.init({
+                // Global settings:
+                disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+                startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+                initClassName: 'aos-init', // class applied after initialization
+                animatedClassName: 'aos-animate', // class applied on animation
+                useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+                disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+                debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+                throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+                              
+                // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+                offset: 120, // offset (in px) from the original trigger point
+                delay: 0, // values from 0 to 3000, with step 50ms
+                duration: 400, // values from 0 to 3000, with step 50ms
+                easing: 'ease', // default easing for AOS animations
+                once: false, // whether animation should happen only once - while scrolling down
+                mirror: false, // whether elements should animate out while scrolling past them
+                anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+              });
         };  
 
     window.addEventListener('DOMContentLoaded', init);
