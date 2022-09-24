6310f79f87fcba54558b16f2157ee40d84d67072
Pedro Martins <snitramordep@gmail.com>
2022-09-24 10:42:30 +0100

    Start responsive styles and add more pub data.

--- js/main.js
+++ js/main.js
@@ -6,10 +6,12 @@
 
         cacheDOM = () => {
             DOM.publications = document.querySelector('.publications');
+            DOM.career = document.querySelector('.career');
+            DOM.scrollTop = document.querySelector('.scrollTop');
         },
 
         UI = {
-            pub: (item) => {
+            pub: (item, index) => {
                 let pub = document.createElement('DIV'),
                     author = document.createElement('SPAN'),
                     year = document.createElement('SPAN'),
@@ -17,13 +19,21 @@
                     source = document.createElement('SPAN'),
                     locale = document.createElement('SPAN');
 
+                pub.className = 'publication';
+                author.className = 'publication__author';
+                source.className = 'publication__source';
+
                 author.textContent = item.author;
                 year.textContent = item.year;
                 title.textContent = item.title;
                 source.textContent = item.pub;
                 locale.textContent = item.locale;
 
-                pub.append(author, ', ', year, '. ', title, source, ', ', locale);
+                pub.append(author, ', ', year, '. ', title, ' ' ,  source, ', ', locale);
+                
+                pub.setAttribute('data-aos', 'fade-up');
+                pub.setAttribute('data-aos-delay', index * 100);
+                pub.setAttribute('data-aos-duration', 1000);
 
                 return pub;
             }
@@ -42,17 +52,53 @@
         },
 
         renderCareer = (data) => {
-            console.log(data);
+            data.forEach((item, i) => {
+                let career = document.createElement('DIV'),
+                    years = document.createElement('DIV'),
+                    content = document.createElement('DIV'),
+                    role =  document.createElement('P'),
+                    institution = document.createElement('P');
+
+                career.className = 'career__item';
+                years.className = 'career__years';
+                content.className = 'career__content';
+                role.className = 'career__role';
+                institution.className = 'career__institution';
+
+                years.textContent = item.years;
+                role.textContent = item.role;
+                institution.textContent = item.institution;
+
+                content.append(role, institution);
+
+                career.append(years, content);
+                career.setAttribute('data-aos', 'fade-up');
+                career.setAttribute('data-aos-duration', '1000');
+                career.setAttribute('data-aos-delay', `${i * 100}`);
+
+                DOM.career.append(career);
+            });
         },
     
         renderPublications = (data) => {
-            data.forEach(pub => {
-                DOM.publications.append(UI.pub(pub));
+            data.forEach((pub, i) => {
+                DOM.publications.append(UI.pub(pub, i));
+            });
+        },
+
+        bindEvents = () => {
+            document.addEventListener('scroll', () => {
+                if (window.scrollY > 600) {
+                    DOM.scrollTop.classList.add('active');
+                } else {
+                    DOM.scrollTop.classList.remove('active');
+                }
             });
         },
 
         init = () => {
             cacheDOM();
+            bindEvents();
             getData();
             AOS.init({
                 // Global settings:
